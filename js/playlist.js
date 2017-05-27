c862fed1982579890c0b0883eee0c0a42602500a
cvb678 <mkmichalk04@gmail.com>
2017-05-27 14:18:36 +0200

    Break

--- js/playlist.js
+++ js/playlist.js
@@ -6,19 +6,16 @@ var songs = [ 'assets/mp3/chopin-spring.mp3',
     'assets/mp3/chopin-mazurka-in-d-major-b71.mp3'
     ];
 
+var backgrounds = [ 'assets/img/bg1.jpg',
+                   'assets/img/bg2.jpg',
+                   'assets/img/bg3.jpg',
+                   'assets/img/bg4.jpg'
+                  ];
+
 var audio = new Audio();
 audio.src = 'assets/mp3/chopin-spring.mp3';
 audio.controls = false;
 audio.autoplay = false;
-
-$(document).ready(function () {
-    $('#player').append(audio);
-    
-    $('li').click(function () { 
-        audio.src=songs[$(this).data("id")];
-    });
-});
-
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
@@ -32,3 +29,12 @@ window.addEventListener('load', function(e) {
 
 }, false);
 
+$(document).ready(function () {
+    $('#player').append(audio);
+    
+    $('li').click(function () {
+        var id = $(this).data("id");
+        audio.src=songs[id];
+        $('#player').css('background-image', 'url(' + '"' + backgrounds[id] + '")');
+    });
+});
